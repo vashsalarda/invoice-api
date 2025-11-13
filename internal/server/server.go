60666ea067ab4ced6d6a6ad8189d0fc1cd0ba18a
Vash Salarda <vash.moonshot@gmail.com>
2025-11-13 11:02:10 +0800

    Move logger to server file

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	"invoice-api/internal/database"
 	auth_route "invoice-api/internal/features/auth/route"
@@ -28,6 +30,19 @@ func New() *FiberServer {
 
 		db: database.New(),
 	}
+
+	server.App.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3001",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: true,
+	}))
+
+	server.App.Use(logger.New(logger.Config{
+		TimeFormat: "2006-01-02 15:04:05",
+		TimeZone:   "Asia/Singapore",
+		Format:     "${blue}[${time}] | ${green}${status} | ${cyan}${latency} | ${blue}${ip} | ${method} | ${white}${path} | ${red}${error}${white}\n",
+	}))
 
 	userRoute := new(user_route.UserRoute)
 	userRoute.Init(server.App)
